900619a0e5196ff02e8621dbe1f8ee85c96c671a
Granvarden <premlekphon@gmail.com>
2023-09-22 17:43:15 +0700

    add comand send pic

--- main.py
+++ main.py
@@ -2,16 +2,17 @@
 
 import discord
 from discord.ext import commands
+#import os เปิดos เพื่อเข้าถึงไฟล์
 
 #เอาไว้สำหรับรอรับคำสั่ง
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 # Run the bot
 # always don't forget to remove token
-
+#os.chdir('โฟลเดอร์รูป')
 def main():
     """Bot"""
-    token = '' #สำหรับรันบอท
+    token = 'MTE1MjYwMDc2NjA1ODYxMDc1OA.G5ErJX.8CAnSf_WdVOwRrnCoBrGPX6OKLyXfHk8azi0NY' #สำหรับรันบอท
 
     @bot.event #ดูสถานะ
     async def on_ready():
@@ -29,6 +30,14 @@ def main():
     async def test(ctx):
         """test"""
         await ctx.send("arai ja!")
+
+    #@bot.command()
+    #async def timeup(ctx): คำสั่งส่งรูป
+    #    
+    #    #ส่งรูปภาพไปยังช่องข้อความ
+    #    await ctx.send('send image', file = discord.File("up.png"))
+
+        
 
     bot.run(token)
 
